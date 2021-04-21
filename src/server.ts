bbe6ebe4cbb93c92e2911bf372613e471dd3ea55
Fabiany Sousa <fabianydesousa@gmail.com>
2021-04-21 20:49:38 -0300

    finishing configuration of project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", ( request, response ) => {
+    return response.json("Olá NLW 05!");
+});
+
+app.post("/users", ( request, response ) => {
+    return response.json("Usuário salvo com sucesso!")
+});
+
+app.listen(3333, () => console.log("Server is running  on port 3333"));
