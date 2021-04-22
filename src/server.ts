e16a8e5bb7dbe316325f78a76f8b119eda4f5f70
Fabiany Sousa <fabianydesousa@gmail.com>
2021-04-22 09:28:17 -0300

    configure typeorm and create migrations, entities, repositories, routes and controller

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express, { request, response } from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", ( request, response ) => {
-    return response.json("Olá NLW 05!");
-});
+app.use(express.json());
 
-app.post("/users", ( request, response ) => {
-    return response.json("Usuário salvo com sucesso!")
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running  on port 3333"));
